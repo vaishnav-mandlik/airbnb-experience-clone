6be15b6210fc9c210406e7d7df0f5a13ee50b701
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2022-11-05 23:04:44 +0530

    More section

---
+++ src/Components/MoreSection/MoreSection.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Card from "./Card";
+
+const MoreSection = () => {
+  return (
+    <Box h="auto" px="80px" w="full">
+      <Card></Card>
+    </Box>
+  );
+};
+
+export default MoreSection;
