de27a3a6f9423c4362c8184074b8d103e6f153d4
Vaishnav Mandlik <vaishnavmandlik@yahoo.com>
2022-11-06 16:51:23 +0530

    collection sec

--- src/Components/Collection/Card.jsx
+++ src/Components/Collection/Card.jsx
@@ -5,15 +5,20 @@ const Card = ({ marg, img, title }) => {
   return (
     <Box>
       <Box
-        w="530px"
-        h="302px"
-        borderRadius="15"
+        w={["280px", "530px", "530px", "530px"]}
+        h={["222px", "302px", "302px", "302px"]}
+        borderRadius={["10", "15", "15", "15"]}
         position="relative"
         textAlign="center"
         ml={5}
         mr={marg}
       >
-        <Image w="530px" h="302px" borderRadius="15" src={img}></Image>
+        <Image
+          w={["280px", "530px", "530px", "530px"]}
+          h={["222px", "302px", "302px", "302px"]}
+          borderRadius="15"
+          src={img}
+        ></Image>
         <Text
           fontSize="12px"
           position="absolute"
@@ -26,7 +31,7 @@ const Card = ({ marg, img, title }) => {
 
         <Text
           position="absolute"
-          fontSize="22px"
+          fontSize={["16px", "22px", "22px", "22px"]}
           fontWeight="500"
           fontFamily="Helvetica Neue"
           color="white"
@@ -41,7 +46,7 @@ const Card = ({ marg, img, title }) => {
             position="absolute"
             backgroundColor="white"
             borderRadius="8"
-            top="250px"
+            top={["170px", "250px", "250px", "250px"]}
             left="25px"
             w="85px"
             h="32px"
