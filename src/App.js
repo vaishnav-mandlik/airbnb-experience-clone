c9de79ff32a0b4bf92ac551e9cf1f6f7b4a051e5
Vaishnav Mandlik <vaishnavmandlik@yahoo.com>
2022-11-06 18:29:14 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<>Home</>} />
           <Route path="/s/experiences/online" element={<Experiences />} />
         </Routes>
       </BrowserRouter>
