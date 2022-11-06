d409e34529a52386810e760340bc434d726cdf76
Vaishnav Mandlik <vaishnavmandlik@yahoo.com>
2022-11-06 16:51:35 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
-import { Box, Button } from "@chakra-ui/react";
-import { useState } from "react";
 import "./App.css";
-import Collection from "./Components/Collection/Collection";
-import MoreSection from "./Components/MoreSection/MoreSection";
-import Navbar from "./Components/Navbar";
-import Sellers from "./Components/Sellers/Sellers";
-import StartingSection from "./Components/StartingSection/StartingSection";
-import Tags from "./Components/Tags/Tags";
-
+import React from "react";
+import Experiences from "./Components/Experiences/Experiences";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
-  const [showMore, setShowMore] = useState(false);
   return (
     <>
-      <Navbar></Navbar>
-      <Collection></Collection>
-      <Tags></Tags>
-      <Sellers
-        endPt="sec2"
-        title="Plan a trip with help from local Hosts around the world"
-        marg={8}
-        smarg={8}
-      ></Sellers>
-      <Sellers endPt="topsec" title="Top sellers"></Sellers>
-      <StartingSection endPt="next-6-hours"></StartingSection>
-
-      <Sellers
-        endPt="groups"
-        title="Great for groups"
-        marg={5}
-        smarg={-40}
-      ></Sellers>
-      <Sellers endPt="weeked-plan" title="Make plans this weekend"></Sellers>
-      <Box justifyContent="center">
-        {showMore && <MoreSection></MoreSection>}
-        <Button
-          backgroundColor="black"
-          ml="550px"
-          color="white"
-          fontSize="14px"
-          fontWeight="600"
-          _hover={{ backgroundColor: "black" }}
-          onClick={() => {
-            setShowMore(true);
-          }}
-          hidden={showMore}
-        >
-          Load More
-        </Button>
-      </Box>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/s/experiences/online" element={<Experiences />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
